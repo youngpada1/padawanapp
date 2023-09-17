a5942b401b5f9f8f3255852474a97d4f606c0f18
Young Padawan <flavsferr@gmail.com>
2023-09-17 17:52:27 +0200

    Updated toml file by adding streamlit

---
+++ padawapp/pages/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#Set layout page
+
+st.set_page_config(layout='wide')
+
+st.write('##')
+st.subheader('Welcome to my app')
+st.title('My Portfolio')
+st.write('[LinkedIn Profile here]{https://www.linkedin.com/in/flaviaferr/}')
