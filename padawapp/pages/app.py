0df2795b86df62d9c95162bf888a4b07bd83a522
Young Padawan <flavsferr@gmail.com>
2023-09-17 19:05:49 +0200

    Updating menu page. Added new dependencies/images

--- padawapp/pages/app.py
+++ padawapp/pages/app.py
@@ -1,10 +1,18 @@
+import pandas as pd
+import glob
 import streamlit as st
+from PIL import imag
 
-#Set layout page
-
+# Set layout page to wide
 st.set_page_config(layout='wide')
 
+
+
+
+# Adding introductory message
 st.write('##')
-st.subheader('Welcome to my app')
-st.title('My Portfolio')
-st.write('[LinkedIn Profile here]{https://www.linkedin.com/in/flaviaferr/}')
+st.subheader('Hi, Welcome to my app') 
+st.title('This is my Portfolio')
+st.write('Linkedin {https://www.linkedin.com/in/flaviaferr/}')
+
+
