0f847f35d01819c3288c07debafbbf5c8579fbda
Young Padawan <flavsferr@gmail.com>
2023-09-28 21:31:17 +0100

    hyperlinks added

--- padawapp/pages/about_me.py
+++ padawapp/pages/about_me.py
@@ -20,33 +20,35 @@ with col1:
     st.image(str(profile_pic), width=100)
 
 # Download resume
+with col2:
     resume = Path('/Users/youngpadawan/flaviapp/padawanapp/padawapp/images/Flavia Ferreira - Resume (IT Management).pdf') #Resume
     #loading resume in PDF Format
     with open(resume, 'rb') as f:
         bytes_data = f.read()
 # Adding download button
     st.download_button( 
-       label='Download my resume',
+       label='Download Resume',
        data=bytes_data,
        file_name='Flavia Ferreira | Resume',
-       mime='application/pdf'
+       mime='application/pdf',
+     
 )
 # Link to Github
 with col3:
-    github = Path(__file__) / '../images/github.png' #GitHub profile
-    st.markdown('''
+    github = 'https://raw.githubusercontent.com/youngpada1/padawanapp/a98515a589676598e3ff4fa18a3c24d6f23971b8/padawapp/images/github.png'
+    st.markdown(f'''
     <a href='https://github.com/youngpada1'>
-        <img src='{github}'/>
+        <img src='{github}' width='20px'/>
     </a>''',
     unsafe_allow_html=True
 )
 
 # Link to LinkedIn
 with col4:
-    linkedin = Path('/Users/youngpadawan/flaviapp/padawanapp/padawapp/images/linkedin.png') #LinkedIn profile
-    st.markdown('''
+    linkedin = 'https://raw.githubusercontent.com/youngpada1/padawanapp/a98515a589676598e3ff4fa18a3c24d6f23971b8/padawapp/images/linkedin.png' #LinkedIn profile
+    st.markdown(f'''
     <a href='https://www.linkedin.com/in/flaviaferr/'>
-        <img src={linkedin}/>
+        <img src='{linkedin}' width='20px'/>
     </a>''',
     unsafe_allow_html=True
 )
