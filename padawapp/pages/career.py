6c4b56d14395f265f307d6e0ee4f9bf2099fb4d2
Young Padawan <flavsferr@gmail.com>
2023-10-06 14:16:57 +0100

    Updated Streamlit, pillow and career page

--- padawapp/pages/career.py
+++ padawapp/pages/career.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_timeline import st_timeline
 
 # Set layout page to wide
 st.set_page_config(layout='wide')
@@ -9,26 +8,14 @@ def career():
     st.title('Professional Experience')
     companies = st.selectbox(
         'Select a company',
-        ('Focusrite','MOD Devices', 'Patch Point', 'Konstruktiv GmbH', 'Koma Elektronik GmbH', 'Winter Modular' 'Befaco' )
+        ('Focusrite','MOD Devices', 'Patch Point', 'Konstruktiv GmbH', 'Koma Elektronik GmbH', 'Winter Modular', 'Befaco' )
 )
     st.write('''___''')
 
-# Timeline 
-    years = [
-       {'Focusrite': 1, 'content': 'Present', 'start': '2021-11-01'},
-       {'MOD Devices': 2, 'content': '2021-07-01', 'start': '2020-07-01'},
-       {'Patch Point': 3, 'content': '2020-04-01', 'start': '2018-09-01'},
-       {'Konstruktiv GmbH': 4, 'content': '2022-10-16', 'start': '2022-10-16'},
-       {'Koma Elektronik GmbH': 5, 'content': '2022-10-25', 'start': '2022-10-25'},
-       {'Winter Modular': 6, 'content': '2022-10-27', 'start': '2022-10-27'},
-       {'Befaco': 7, 'content': '2022-10-27', 'start': '2022-10-27'}
-]
-    timeline = st_timeline(years, groups=[], options={}, height='150px')
-   
 # Experience
     if companies == 'Focusrite':
-        title = st.markdown('Production Engineer - Focusrite Audio LTD')
-        experience = st.text('''
+        title = st.markdown('Production Engineer')
+        focusrite = st.text('''
                                   PLM System Trainee
 - Identified process enhancement opportunities during organizational expansion.
 - Results after the first year:
@@ -66,7 +53,71 @@ Cost Management
 Design Optimization
 - Focused on implementing Design for Manufacturing (DFM) and Design for Assembly (DFA) principles to optimize manufacturability and assembly processes, enhancing efficiency and standardization.''')
         
+    elif companies == 'MOD Devices':
+        title = st.markdown('Production Manager')
+        mod_devices = st.text('''
+- Strategically planned, coordinated, and meticulously documented production and quality control processes across multiple departments.
+- Demonstrated expertise in diagnosing and resolving technical issues, contributing to efficient problem-solving and minimizing production downtime.
+- Proficiently debugged Linux-based hardware systems, diligently documenting issues and ensuring seamless releases to the factories.
+- Executed the construction and rigorous validation of prototypes vital for hardware development, specifically New Product Introductions (NPI).
+- Proactively identified and communicated potential risks to stakeholders, fostering timely mitigation to prevent production delays and issues, all while championing a culture of continuous process improvement.
+- Collaborated effectively with external service providers to facilitate the seamless transition of products into Mass Production.
+- Managed logistics operations encompassing production, fulfillment, and comprehensive customer support to ensure operational excellence.
+''')
     
+    elif companies == 'Patch Point':
+        title = st.markdown('Production Manager')
+        patch_point = st.text('''
+During my time at Patch Point, I had the privilege of contributing to several high-profile projects. Among these accomplishments, I take particular pride in overseeing the construction of all Serge systems and the TTSH for the University of New York (US).My responsibilities at Patch Point encompassed a diverse range of critical tasks, demonstrating my comprehensive skill set and commitment to excellence:
+Methodically documented and established streamlined processes to ensure operational efficiency.
+- Led a team of personnel, providing supervision and comprehensive training to ensure optimal performance.
+- Applied technical expertise in soldering, assembly, testing, and precision calibration of intricate instruments.
+- Managed the procurement of components and production materials, ensuring seamless workflow.
+- Leveraged problem-solving skills to troubleshoot hardware issues and facilitate swift resolutions.
+- Delivered the implementation of an inventory management system, optimising production line organisation for enhanced productivity.
+
+''')
+        
+    elif companies == 'Koma Elektronik GmbH':
+        title = st.markdown('Manufacturing Technican')
+        koma = st.text('''
+- Proficiently performed comprehensive product assembly of boutique audio equipment.
+- Demonstrated expertise in diagnosing and resolving technical issues.
+- Conducted rigorous quality control assessments and meticulous testing procedures. 
+''')
+    
+    elif companies == 'Konstruktiv GmbH':
+        title = st.markdown('Testing Engineer')
+        konstruktiv = st.text('''
+
+- Assembled prototypes for external companies, ensuring precision and attention to detail in the manufacturing process.
+- Conducted manufacturability assessments to optimize product design and production efficiency, resulting in cost savings and improved product quality.
+- Proficiently performed comprehensive product assembly of boutique audio equipment, meeting or exceeding quality standards and production timelines.
+- Demonstrated expertise in diagnosing and resolving technical issues, contributing to efficient problem-solving and minimizing production downtime.
+- Conducted rigorous quality control assessments to maintain high product standards and customer satisfaction.
+- Implemented meticulous testing procedures to validate product performance and functionality, ensuring reliability and adherence to specifications
+
+''')
+        
+    elif companies == 'Winter Modular':
+        title = st.markdown('Manufacturing Technician')
+        winter_modular = st.text('''
+- Played a pivotal role in the successful delivery of 250 units for the first release within a tight two-week timeframe. This involved active participation in product assembly, quality assurance, and packaging.
+- Demonstrated exceptional problem-solving skills while working tirelessly to overcome production challenges, such as the faceplate not fitting well with the PCB, despite the absence of documentation.
+- Proficiently performed comprehensive product assembly, ensuring the highest standards of quality and craftsmanship.
+- Showcased expertise in diagnosing and effectively resolving technical issues, contributing to the seamless production process.
+- Conducted rigorous quality control assessments and implemented meticulous testing procedures, upholding stringent quality standards throughout the production cycle.
+                                 ''')
+    
+    elif companies == 'Befaco':
+        title = st.markdown('Manufacturing Technican')
+        befaco = st.text('''
+- Started my career in the audio tech industry as a novice and quickly gained proficiency in comprehensive product assembly.
+- Demonstrated expertise in diagnosing and effectively resolving technical issues, contributing to smooth production processes.
+- Conducted thorough quality control assessments and implemented meticulous testing procedures, upholding the company's commitment to product excellence.
+
+''')
+
     st.write()
 
 career()
