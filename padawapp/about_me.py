9af941635c96e46238345af858a702cd4cf84b5c
Young Padawan <flavsferr@gmail.com>
2023-12-14 22:21:15 +0000

    full path for resume

--- padawapp/about_me.py
+++ padawapp/about_me.py
@@ -24,7 +24,7 @@ def about_me():
 
         # Download resume
         with col2:
-            resume = Path('images/Menu/FlaviaFerreira.pdf') #Resume
+            resume = Path(__file__).parent / Path('images/Menu/FlaviaFerreira.pdf') #Resume
             #loading resume in PDF Format
             with open(resume, 'rb') as f:
                 bytes_data = f.read()
